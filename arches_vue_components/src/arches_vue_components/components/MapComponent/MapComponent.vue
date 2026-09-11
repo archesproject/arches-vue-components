d3ce1c104b0fa64f3587b6949df435a6aadf072a
Alexei Peters <apeters@fargeo.com>
2026-09-11 12:19:11 -0700

    fix default open state for the interaction drawer, re #54

--- arches_vue_components/src/arches_vue_components/components/MapComponent/MapComponent.vue
+++ arches_vue_components/src/arches_vue_components/components/MapComponent/MapComponent.vue
@@ -105,7 +105,7 @@ provide(mapContextKey, context);
             position="right"
             :context="context"
             :items="resolvedInteractionTools"
-            :default-open-index="0"
+            :default-open-index=undefined
         />
         <Toast group="map-component" />
     </div>
