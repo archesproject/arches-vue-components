42f37f5960a7e4ed73c1694a921a5567b76facc7
Christopher Byrd <chrabyrd@gmail.com>
2026-09-18 14:46:01 -0700

    prettier

--- arches_vue_components/src/arches_vue_components/datatypes/resource-instance/types.ts
+++ arches_vue_components/src/arches_vue_components/datatypes/resource-instance/types.ts
@@ -21,7 +21,8 @@ export interface ResourceInstanceSelectOption {
     resource_id: string;
 }
 
-export interface ResourceInstanceCardXNodeXWidgetData extends CardXNodeXWidgetData {
+export interface ResourceInstanceCardXNodeXWidgetData
+    extends CardXNodeXWidgetData {
     node: CardXNodeXWidgetData["node"] & {
         config: {
             graphs?:
--- arches_vue_components/src/arches_vue_components/widgets/ResourceInstanceMultiselectWidget/types.ts
+++ arches_vue_components/src/arches_vue_components/widgets/ResourceInstanceMultiselectWidget/types.ts
@@ -3,7 +3,8 @@ import type { CardXNodeXWidgetData } from "@/arches_vue_components/types.ts";
 import type { ResourceInstanceListAliasedNodeData } from "@/arches_vue_components/datatypes/resource-instance-list/types.ts";
 import type { ResourceInstanceReference } from "@/arches_vue_components/datatypes/resource-instance/types";
 
-export interface ResourceInstanceMultiselectWidgetProps extends BaseWidgetProps {
+export interface ResourceInstanceMultiselectWidgetProps
+    extends BaseWidgetProps {
     cardXNodeXWidgetData?: CardXNodeXWidgetData;
     aliasedNodeData?: ResourceInstanceListAliasedNodeData | null;
     value?: ResourceInstanceReference[] | null;
